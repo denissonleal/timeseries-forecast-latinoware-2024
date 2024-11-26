d0bb1667b29f5b77b7d77b5c02f393d6ca7cdacb
Denisson Leal <denissonleal@gmail.com>
2024-11-26 17:48:18 +0000

    apresentacao latinoware 2024

---
+++ libs/utils.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def build_timeseries(mat, y_col_index, TIME_STEPS):
+    dim_0 = mat.shape[0] - TIME_STEPS
+    dim_1 = mat.shape[1]
+    x = np.zeros((dim_0, TIME_STEPS, dim_1))
+    y = np.zeros((dim_0,))
+
+    for i in range(dim_0):
+        x[i] = mat[i:TIME_STEPS+i]
+        y[i] = mat[TIME_STEPS+i, y_col_index]
+    print("length of time-series i/o",x.shape,y.shape)
+    return x, y
+
+
+def trim_dataset(mat, batch_size):
+    """
+    trims dataset to a size that's divisible by BATCH_SIZE
+    """
+    no_of_rows_drop = mat.shape[0]%batch_size
+    if(no_of_rows_drop > 0):
+        return mat[:-no_of_rows_drop]
+    else:
+        return mat
